b4cd9c76c3b7010ea32e066ab132400f62c044b2
evenflow58 <evenflow58@gmail.com>
2023-07-10 07:59:54 -0400

    New alias

--- api/v1/test/get/src/main.rs
+++ api/v1/test/get/src/main.rs
@@ -13,7 +13,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let resp = Response::builder()
         .status(200)
         .header("content-type", "application/json")
-        .body("Hello World - new version".into())
+        .body("Hello World - new alias".into())
         .map_err(Box::new)?;
     Ok(resp)
 }

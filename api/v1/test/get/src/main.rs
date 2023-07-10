bccfe27a7e95909308dd18cdf6e2b30b3d02ba9d
evenflow58 <evenflow58@gmail.com>
2023-07-10 07:38:40 -0400

    New version

--- api/v1/test/get/src/main.rs
+++ api/v1/test/get/src/main.rs
@@ -13,7 +13,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let resp = Response::builder()
         .status(200)
         .header("content-type", "application/json")
-        .body("Hello World - version 1 - new alias".into())
+        .body("Hello World - new version".into())
         .map_err(Box::new)?;
     Ok(resp)
 }

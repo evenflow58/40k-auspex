f224cb642a614929ee572201de297a6a29409349
evenflow58 <evenflow58@gmail.com>
2023-07-29 13:21:52 -0400

    Add authorizer (#23)
    * Add authorizer
    
    * Use secret string
    
    * Trying to get the arn
    
    * Back to id
    
    * Remove Auth creds
    
    * Added authorization to v1 get
    
    * Adding wildcard domain name
    
    * Remove UI from build
    
    * Added authorizer
    
    * Fixed typo
    
    * Trying build graph
    
    * Updated build spec
    
    * Reworked build spec
    
    * Rework build specs
    
    * Trying different file location
    
    * Rework buildspec
    
    * Add . for location and add debugging
    
    * Moved property
    
    * Moving all build specs to same folder
    
    * Used full path
    
    * Using inline build
    
    * Updated build spec
    
    * Updated build matrix
    
    * Trying to better navigate
    
    * Further navigation
    
    * Update api-build.yaml
    
    * Where am I
    
    * Using build-list
    
    * Adding another where am I?
    
    * Removed api
    
    * Wrapping in brackets
    
    * printing env variables
    
    * Print all env variables
    
    * Try without shell
    
    * Moving printenv to build
    
    * Rework buildspec
    
    * Moved to separate file
    
    * Un-nesting builds
    
    * Removed dependency
    
    * Add actual commands and outputs
    
    * Added openssl and removed unnecessary commands
    
    * Trying to get artifacts
    
    * Add all files as outputs
    
    * Fix output name
    
    * Export all files
    
    * Add secondary artifacts
    
    * Add secondary artifacts
    
    * Change secondary build output
    
    * Use env variable in path
    
    * Target specific files and add all build
    
    * Get all files
    
    * Run as list
    
    * Add version to buildspec
    
    * Add install
    
    * Moved back to graph
    
    * Only use one step
    
    * Go back to using build all
    
    * Removed ui from build
    
    * Add ui back in
    
    * Fix build spec
    
    * Deploy to S3
    
    * Don't package ui
    
    * Fix authorizer
    
    * Use GetAtt
    
    * Moved DependsOn property
    
    * Add role
    
    * Fixed CFT type
    
    * Ignore v2 for now
    
    * Updated auth
    
    * Added logging
    
    * Fix build errors
    
    * Handle string return
    
    * Remove serialization to get response
    
    * Just get error
    
    * Removed too much code
    
    * Just get the json
    
    * More logging
    
    * Use text to get body
    
    * Using json
    
    * Put back full function
    
    * Removed unused variable
    
    * Get event to be able to run lambda
    
    * Adding debugging
    
    * Using straight asterixes
    
    * Better query string usage
    
    * Add beta features
    
    * Trying to limit package size
    
    * Use arm64
    
    * Move back to using cargo build
    
    * Move variables to env variables
    
    * Fixed ENV variables in template
    
    * Get value from ssm
    
    * Fix policy
    
    * Fix action
    
    * Log audience
    
    * Don't cache responses for testing purposes
    
    * Set decrypt to true
    
    * Added logging
    
    * Removed unnecessary log
    
    * Able to deploy endpoints from SAM. Checking deployment from build
    
    * Build auth old way
    
    * Changed CodeUri
    
    * Change CodeUri to a parameter
    
    * Use cargo lambda
    
    * Change build command
    
    * Checking path
    
    * Added cd
    
    * Try again
    
    * Mimic V2
    
    * Use string
    
    * Trying double quotes
    
    * Remove extra quote
    
    * Set back to simple string
    
    * Finish up auth work

--- api/v1/test/get/app/src/main.rs
+++ api/v1/test/get/app/src/main.rs
@@ -5,7 +5,7 @@ use lambda_http::{run, service_fn, Body, Error, Request, Response};
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
-    println!("{:?}", event);
+    println!("{:#?}", event);
     // Extract some useful information from the request
 
     // Return something that implements IntoResponse.
@@ -13,7 +13,7 @@ async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     let resp = Response::builder()
         .status(200)
         .header("content-type", "application/json")
-        .body("Hello World - actual version 2".into())
+        .body("Hello World - using sam deploy".into())
         .map_err(Box::new)?;
     Ok(resp)
 }
---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,74 @@
+<script>
+	import Header from './Header.svelte';
+	import jwt_decode from 'jwt-decode';
+	import './styles.css';
+
+	const initGoogle = () => {
+		debugger
+		const handleGoogleCallback = ({ credential }) => {
+			debugger
+			const test = jwt_decode(credential);
+			console.log(test);
+		}
+		
+		google.accounts.id.initialize({
+			client_id: '181396477895-mif6hcekhvhi32up28g49hve07vlvchm.apps.googleusercontent.com',
+			auto_select: true,
+			callback: handleGoogleCallback
+		});
+		google.accounts.id.prompt(	);
+	}
+</script>
+
+<svelte:head>
+	<script src="https://accounts.google.com/gsi/client" async on:load={initGoogle}></script>
+</svelte:head>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 64rem;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 12px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 12px 0;
+		}
+	}
+</style>

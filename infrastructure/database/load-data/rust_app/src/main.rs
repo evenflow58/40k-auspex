2b00b3f134e118373245359c45bd3b96316e4457
evenflow58 <evenflow58@gmail.com>
2023-09-10 19:41:58 -0400

    Changing to info

--- infrastructure/database/load-data/rust_app/src/main.rs
+++ infrastructure/database/load-data/rust_app/src/main.rs
@@ -3,7 +3,7 @@ use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 // use aws_sdk_dynamodb::{Client};
 // use aws_sdk_dynamodb::{types::{WriteRequest, PutRequest, AttributeValue}};
 use envmnt;
-use tracing::log::{debug};
+use tracing::log::{info};
 
 // use load_data::models::{response::Response, request::Request, army::Army};
 use load_data::models::{response::Response, request::Request};
@@ -14,7 +14,7 @@ use load_data::models::{response::Response, request::Request};
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::DEBUG)
+        .with_max_level(tracing::Level::INFO)
         // disable printing the name of the module in every log line.
         .with_target(false)
         // disabling time is handy because CloudWatch will add the ingestion time.
@@ -27,11 +27,11 @@ async fn main() -> Result<(), Error> {
 // Create a function call function_handler that takes a LambdaEvent and returns a Result
 // with a Response or an Error
 async fn function_handler(_event: LambdaEvent<Request>) -> Result<Response, Error> {
-    debug!("Starting");
+    info!("Starting");
 
     // // Create a variable called file that opens the file data/grey_knights.json
     // let grey_knights: Army = serde_json::from_str(&String::from_utf8_lossy(include_bytes!("data/grey_knights.json")))?;
-    // debug!("Grey knights serialized {:?}", grey_knights);
+    // info!("Grey knights serialized {:?}", grey_knights);
     // // Create a variable called reader that creates a BufReader from the file variable
     // // let reader = BufReader::new(file);
     // // Create a variable called grey_knights that is a Army struct that is created from
@@ -41,11 +41,11 @@ async fn function_handler(_event: LambdaEvent<Request>) -> Result<Response, Erro
     // // Create a variable called config that is a aws_config::Config that is created from
     // // the load_from_env() function
     // let config = ::aws_config::load_from_env().await;
-    // debug!("Loaded config {:?}", config);
+    // info!("Loaded config {:?}", config);
     // // Create a variable called client that is a dynamodb::Client that is created from
     // // the config variable
     // let client = Client::new(&config);
-    // debug!("Created client");
+    // info!("Created client");
 
     // // Create a variable called table_name that is a String that is created from the
     // // TABLE_NAME environment variable

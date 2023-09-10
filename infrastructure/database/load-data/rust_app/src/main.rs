597063a9afdd215d8fcc177ce118c2646534b0a3
evenflow58 <evenflow58@gmail.com>
2023-09-10 18:46:19 -0400

    Stripping it down

--- infrastructure/database/load-data/rust_app/src/main.rs
+++ infrastructure/database/load-data/rust_app/src/main.rs
@@ -1,11 +1,15 @@
 use std::collections::HashMap;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
-use aws_sdk_dynamodb::{Client};
-use aws_sdk_dynamodb::{types::{WriteRequest, PutRequest, AttributeValue}};
+// use aws_sdk_dynamodb::{Client};
+// use aws_sdk_dynamodb::{types::{WriteRequest, PutRequest, AttributeValue}};
 use envmnt;
 use tracing::log::{debug};
 
-use load_data::models::{response::Response, request::Request, army::Army};
+// use load_data::models::{response::Response, request::Request, army::Army};
+use load_data::models::{response::Response, request::Request};
+
+// aws-config = "0.55.3"
+// aws-sdk-dynamodb = "0.28.0"
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -25,62 +29,62 @@ async fn main() -> Result<(), Error> {
 async fn function_handler(_event: LambdaEvent<Request>) -> Result<Response, Error> {
     debug!("Starting");
 
-    // Create a variable called file that opens the file data/grey_knights.json
-    let grey_knights: Army = serde_json::from_str(&String::from_utf8_lossy(include_bytes!("data/grey_knights.json")))?;
-    debug!("Grey knights serialized {:?}", grey_knights);
-    // Create a variable called reader that creates a BufReader from the file variable
-    // let reader = BufReader::new(file);
-    // Create a variable called grey_knights that is a Army struct that is created from
-    // the reader variable
-    // let grey_knights: Army = serde_json::from_reader(reader).unwrap();
+    // // Create a variable called file that opens the file data/grey_knights.json
+    // let grey_knights: Army = serde_json::from_str(&String::from_utf8_lossy(include_bytes!("data/grey_knights.json")))?;
+    // debug!("Grey knights serialized {:?}", grey_knights);
+    // // Create a variable called reader that creates a BufReader from the file variable
+    // // let reader = BufReader::new(file);
+    // // Create a variable called grey_knights that is a Army struct that is created from
+    // // the reader variable
+    // // let grey_knights: Army = serde_json::from_reader(reader).unwrap();
 
-    // Create a variable called config that is a aws_config::Config that is created from
-    // the load_from_env() function
-    let config = ::aws_config::load_from_env().await;
-    debug!("Loaded config {:?}", config);
-    // Create a variable called client that is a dynamodb::Client that is created from
-    // the config variable
-    let client = Client::new(&config);
-    debug!("Created client");
+    // // Create a variable called config that is a aws_config::Config that is created from
+    // // the load_from_env() function
+    // let config = ::aws_config::load_from_env().await;
+    // debug!("Loaded config {:?}", config);
+    // // Create a variable called client that is a dynamodb::Client that is created from
+    // // the config variable
+    // let client = Client::new(&config);
+    // debug!("Created client");
 
-    // Create a variable called table_name that is a String that is created from the
-    // TABLE_NAME environment variable
-    let table_name = envmnt::get_or_panic("TABLE_NAME").to_string();
+    // // Create a variable called table_name that is a String that is created from the
+    // // TABLE_NAME environment variable
+    // let table_name = envmnt::get_or_panic("TABLE_NAME").to_string();
 
-    // Using the client variable call the batch_write_item() function with the
-    // table_name variable and a HashMap with the id of "grey_knights" and the
-    // type of "army" and the data of the grey_knights variable.
-    client
-        .batch_write_item()
-        .request_items(
-            table_name,
-            vec![
-                WriteRequest::builder()
-                    .put_request(
-                        PutRequest::builder()
-                            .set_item(Some(HashMap::from(
-                                [
-                                    (
-                                        "id".to_string(),
-                                        AttributeValue::S("GreyKnights".to_string())
-                                    ),
-                                    (
-                                        "type".to_string(),
-                                        AttributeValue::S("Army".to_string())
-                                    ),
-                                    (
-                                        "data".to_string(),
-                                        AttributeValue::M(grey_knights.get_hash_map())
-                                    )
-                                ]
-                            )))
-                            .build()
-                    )
-                    .build()
-            ],
-        )
-        .send()
-        .await?;
+    // // Using the client variable call the batch_write_item() function with the
+    // // table_name variable and a HashMap with the id of "grey_knights" and the
+    // // type of "army" and the data of the grey_knights variable.
+    // client
+    //     .batch_write_item()
+    //     .request_items(
+    //         table_name,
+    //         vec![
+    //             WriteRequest::builder()
+    //                 .put_request(
+    //                     PutRequest::builder()
+    //                         .set_item(Some(HashMap::from(
+    //                             [
+    //                                 (
+    //                                     "id".to_string(),
+    //                                     AttributeValue::S("GreyKnights".to_string())
+    //                                 ),
+    //                                 (
+    //                                     "type".to_string(),
+    //                                     AttributeValue::S("Army".to_string())
+    //                                 ),
+    //                                 (
+    //                                     "data".to_string(),
+    //                                     AttributeValue::M(grey_knights.get_hash_map())
+    //                                 )
+    //                             ]
+    //                         )))
+    //                         .build()
+    //                 )
+    //                 .build()
+    //         ],
+    //     )
+    //     .send()
+    //     .await?;
 
     Ok(Response {})
 }
--- infrastructure/database/load-data/rust_app/src/models/mod.rs
+++ infrastructure/database/load-data/rust_app/src/models/mod.rs
@@ -1,4 +1,4 @@
-pub mod unit;
-pub mod army;
+// pub mod unit;
+// pub mod army;
 pub mod request;
 pub mod response;

a1250451ad1e53bbc1788ce93341ffdf095fcf9b
evenflow58 <evenflow58@gmail.com>
2023-09-11 07:22:05 -0400

    Add back full function

--- infrastructure/database/load-data/rust_app/src/main.rs
+++ infrastructure/database/load-data/rust_app/src/main.rs
@@ -40,47 +40,47 @@ async fn function_handler(_event: LambdaEvent<Request>) -> Result<Response, Erro
     info!("Loaded config {:?}", config);
     // // Create a variable called client that is a dynamodb::Client that is created from
     // // the config variable
-    // let client = Client::new(&config);
-    // info!("Created client");
+    let client = Client::new(&config);
+    info!("Created client");
 
-    // // Create a variable called table_name that is a String that is created from the
-    // // TABLE_NAME environment variable
-    // let table_name = envmnt::get_or_panic("TABLE_NAME").to_string();
+    // Create a variable called table_name that is a String that is created from the
+    // TABLE_NAME environment variable
+    let table_name = envmnt::get_or_panic("TABLE_NAME").to_string();
 
-    // // Using the client variable call the batch_write_item() function with the
-    // // table_name variable and a HashMap with the id of "grey_knights" and the
-    // // type of "army" and the data of the grey_knights variable.
-    // client
-    //     .batch_write_item()
-    //     .request_items(
-    //         table_name,
-    //         vec![
-    //             WriteRequest::builder()
-    //                 .put_request(
-    //                     PutRequest::builder()
-    //                         .set_item(Some(HashMap::from(
-    //                             [
-    //                                 (
-    //                                     "id".to_string(),
-    //                                     AttributeValue::S("GreyKnights".to_string())
-    //                                 ),
-    //                                 (
-    //                                     "type".to_string(),
-    //                                     AttributeValue::S("Army".to_string())
-    //                                 ),
-    //                                 (
-    //                                     "data".to_string(),
-    //                                     AttributeValue::M(grey_knights.get_hash_map())
-    //                                 )
-    //                             ]
-    //                         )))
-    //                         .build()
-    //                 )
-    //                 .build()
-    //         ],
-    //     )
-    //     .send()
-    //     .await?;
+    // Using the client variable call the batch_write_item() function with the
+    // table_name variable and a HashMap with the id of "grey_knights" and the
+    // type of "army" and the data of the grey_knights variable.
+    client
+        .batch_write_item()
+        .request_items(
+            table_name,
+            vec![
+                WriteRequest::builder()
+                    .put_request(
+                        PutRequest::builder()
+                            .set_item(Some(HashMap::from(
+                                [
+                                    (
+                                        "id".to_string(),
+                                        AttributeValue::S("GreyKnights".to_string())
+                                    ),
+                                    (
+                                        "type".to_string(),
+                                        AttributeValue::S("Army".to_string())
+                                    ),
+                                    (
+                                        "data".to_string(),
+                                        AttributeValue::M(grey_knights.get_hash_map())
+                                    )
+                                ]
+                            )))
+                            .build()
+                    )
+                    .build()
+            ],
+        )
+        .send()
+        .await?;
 
     Ok(Response {})
 }

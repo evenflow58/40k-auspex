fec023e8f5d142ef8e9f51080ee51c6f3920db22
evenflow58 <evenflow58@gmail.com>
2023-10-06 22:27:42 -0400

    Add error handling

--- processing/drop-files/rust_app/src/main.rs
+++ processing/drop-files/rust_app/src/main.rs
@@ -1,13 +1,23 @@
 use aws_lambda_events::event::codepipeline_job::CodePipelineJobEvent;
 use aws_sdk_s3::Client as s3_sdk_client;
+
+use aws_sdk_s3::operation::put_object::PutObjectOutput;
+use aws_sdk_s3::error::SdkError;
+use aws_sdk_s3::operation::put_object::PutObjectError;
+
 use aws_sdk_codepipeline::Client as codepipeline_job_sdk_client;
+use aws_sdk_codepipeline::types::FailureDetails;
+use aws_sdk_codepipeline::types::FailureType;
+
 use futures::future::join_all;
 use lambda_runtime::{run, service_fn, Error, LambdaEvent};
 use tracing::info;
 use include_dir::Dir;
 use bytes::Bytes;
-use aws_smithy_http::byte_stream::ByteStream;
 
+use aws_smithy_http::byte_stream::ByteStream;
+use aws_smithy_runtime_api::client::orchestrator::HttpResponse;
+use aws_smithy_types::error::display::DisplayErrorContext;
 
 use drop_files::models::response::Response;
 
@@ -45,10 +55,15 @@ async fn function_handler(event: LambdaEvent<CodePipelineJobEvent>) -> Result<Re
 
     let mut futures = Vec::new();
 
+    let job_id = event.payload.code_pipeline_job.id;
+
     // Loop through the DATA_DIR.files() and put them into the S3 bucket
     let bucket = event.payload.code_pipeline_job.data.action_configuration.configuration.user_parameters.unwrap();
     for file in DATA_DIR.files() {
         let key = file.path().to_str().unwrap();
+
+        info!("Uploading {:?}", key);
+
         let body = ByteStream::from(Bytes::from_static(file.contents()));
         futures.push(s3_client
             .put_object()
@@ -58,14 +73,38 @@ async fn function_handler(event: LambdaEvent<CodePipelineJobEvent>) -> Result<Re
             .send());
     }
 
-    // await futures
-    join_all(futures).await;
+    info!("Waiting for futures");
 
-    codepipeline_job_client
-        .put_job_success_result()
-        .set_job_id(event.payload.code_pipeline_job.id)
-        .send()
-        .await?;
+    // await futures
+    let results: Vec<Result<PutObjectOutput, SdkError<PutObjectError, HttpResponse>>> = join_all(futures).await.into_iter().collect();
+
+    for result in results {
+        match result {
+            Ok(_) => {
+                info!("Futures complete");
+                codepipeline_job_client
+                    .put_job_success_result()
+                    .set_job_id(job_id.clone())
+                    .send()
+                    .await?;
+            },
+            Err(error) => {
+                info!("Futures failed {:?}", DisplayErrorContext(&error));
+
+                let failure_details = FailureDetails::builder()
+                    .message(format!("{}", DisplayErrorContext(&error)))
+                    .set_type(Some(FailureType::JobFailed))
+                    .build();
+
+                codepipeline_job_client
+                    .put_job_failure_result()
+                    .set_job_id(job_id.clone())
+                    .failure_details(failure_details)
+                    .send()
+                    .await?;
+            }
+        }
+    }
 
     Ok(Response {})
 }

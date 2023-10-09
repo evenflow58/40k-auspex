57e76414861828f641937dbbad79dfaf52e3e938
evenflow58 <evenflow58@gmail.com>
2023-10-09 19:51:31 -0400

    Get query string parameters

--- api/v1/army/get_all/rust_app/src/main.rs
+++ api/v1/army/get_all/rust_app/src/main.rs
@@ -1,29 +1,30 @@
-use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use lambda_http::{run, service_fn, Error, Request, Response, Body, RequestExt};
+use http::HeaderMap;
 
 /// This is the main body for the function.
 /// Write your code inside it.
 /// There are some code example in the following URLs:
 /// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    println!("Event: {:#?}", event);
+    println!("Params: {:#?}", event.query_string_parameters_ref());
     // Extract some useful information from the request
-    let who = event
-        .query_string_parameters_ref()
-        .and_then(|params| params.first("name"))
-        .unwrap_or("world");
-    let message = format!("Hello {who}, this is an AWS Lambda HTTP request");
 
-    // Return something that implements IntoResponse.
-    // It will be serialized to the right response event automatically by the runtime
+    let mut headers = HeaderMap::new();
+    headers.insert("content-type", "application/json".parse().unwrap());
+
     let resp = Response::builder()
         .status(200)
-        .header("content-type", "text/html")
-        .body(message.into())
+        .header("content-type", "application/json")
+        .body("Some response".into())
         .map_err(Box::new)?;
+
     Ok(resp)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    env_logger::init();
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
         // disable printing the name of the module in every log line.
